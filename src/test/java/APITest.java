cac05398355fd16a5405e47a7f21939f36db4eff
Josinaldo Júnior <josinaldo_jr@hotmail.com>
2021-01-06 12:58:40 -0300

    Teste de remocao

--- src/test/java/APITest.java
+++ src/test/java/APITest.java
@@ -47,4 +47,23 @@ public class APITest {
         ;
     }
 
+    @Test
+    public void deveRemoverTarefa(){
+        Integer id =RestAssured.given()
+                .body("  {\"task\":\"Teste de remocao\", \"dueDate\":\"2022-01-01\"}")
+                .contentType(ContentType.JSON)
+                .when()
+                .post("/todo")
+                .then()
+                .statusCode(201)
+                .extract().path("id")
+        ;
+        RestAssured.given()
+                .when()
+                    .delete("/todo/"+id)
+                .then()
+                    .statusCode(204)
+                ;
+    }
+
 }
