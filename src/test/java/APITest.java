1ca3d71b1076711333e800093ddc6110a5ed45d7
Josinaldo Júnior <josinaldo_jr@hotmail.com>
2021-04-13 21:39:22 -0300

    Versao 8

--- src/test/java/APITest.java
+++ src/test/java/APITest.java
@@ -29,7 +29,7 @@ public class APITest {
                 .when()
                 .post("/todo")
                 .then()
-                .statusCode(200)
+                .statusCode(201)
         ;
     }
 
